01c97c98f22de03a81f2250d2e701bcadfc379a4
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-09 18:42:13 -0400

    Prep Major & Minor key
    Set up separate major and minor key value for easy switching

--- Song_Generator.ino
+++ Song_Generator.ino
@@ -12,10 +12,14 @@ int minor[] = {
   NOTE_GS4, NOTE_C5, NOTE_D5, NOTE_F5,
   NOTE_G5, NOTE_GS5, NOTE_C6, NOTE_D6};
 
+int durMaj = 160;
+int durMin = 180;
 int duration;
 int randNote;
 int randDur;
 int songFlow = 0;
+
+char key = 'a';
  
 void setup() {
   randomSeed(analogRead(0));
@@ -25,25 +29,26 @@ void loop() {
   randNote = random(8);
   randDur = random(8);
   songFlow += randDur;
-
+  
+  duration = (key == 'a' ? durMaj : durMin) 
+  * (songFlow < 200 ? random(3)+1 : 1);
+  
   if(songFlow > 350){
     songFlow = 0;
   }
-  
-  if(randDur < 2 || songFlow > 150){
-    duration = 180;
+
+  if(key == 'a'){
+    tone(8, major[randNote], duration > durMaj ?  durMin*2 : durMaj);
+  }else{
+    tone(8, minor[randNote], duration > durMin ?  durMin*2 : durMin);
   }
-  else if(randDur == 2){
-    duration = 720;
-  }
-  else{
-    duration = 360;
-  }
-  
-  tone(8, minor[randNote], duration == 180 ? 180 : 360);
   delay(duration);
 }
 
 void minorSwitch(){
-  
+  if(key == 'a'){
+    key == 'i';
+  }else{
+    key == 'a';
+  }
 }
